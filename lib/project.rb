85331f17fa3638b9fef97bad78c508ba43e0621b
Michael Penn <mwpenn94@gmail.com>
2021-02-25 03:04:52 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 class Project
-	attr_accessor	:name, :backers
+	attr_accessor	:title, :backers
 
 	@@all = []
 
-	def initialize(name)
-	  @name = name
+	def initialize(title)
+	  @title = title
 		@backers = []
 		save
 	end
